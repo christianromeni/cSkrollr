54af53fa1c51bfe87523788975eeccee4295f812
Christian Romeni <c.romeni@brainfactory.de>
2015-07-14 10:28:20 +0200

    moved classes to classes folder, added english translation

--- config/autoload.php
+++ config/autoload.php
@@ -28,9 +28,8 @@ ClassLoader::addNamespaces(array
 ClassLoader::addClasses(array
 (
 	// Elements
-	// 'cSkrollr\cSkrollrElement' => 'system/modules/cSkrollr/elements/cSkrollrElement.php',
-	'cSkrollr\DataAttributes' => 'system/modules/cSkrollr/elements/DataAttributes.php',
-	'cSkrollr\cSkrollrHook' => 'system/modules/cSkrollr/elements/cSkrollrHook.php',
+	'cSkrollr\DataAttributes' => 'system/modules/cSkrollr/classes/DataAttributes.php',
+	'cSkrollr\cSkrollrHook' => 'system/modules/cSkrollr/classes/cSkrollrHook.php',
 ));
 
 
---
+++ languages/en/tl_article.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @package   BcatExtras 
+ * @author    Yuriy Davats 
+ * @license   GNU 
+ * @copyright die praxis 
+ */
+
+$GLOBALS['TL_LANG']['tl_article']['cskrollr_legend'] = 'cSkrollr Settings';
+
+$GLOBALS['TL_LANG']['tl_article']['cskrollr_attributes'][0] = 'cSkrollr data-Fields';
+$GLOBALS['TL_LANG']['tl_article']['cskrollr_attributes'][1] = '';
